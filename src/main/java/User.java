dc3fafbe17e407cf525c5a195623d154180ca209
chenkangqiang <chenkangqiang@meituan.com>
2017-07-05 17:10:24 +0800

    add files

---
+++ src/main/java/User.java
@@ -0,0 +1,18 @@
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Created by chen on 2017/7/5.
+ */
+@Data
+@Log4j2
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public static void main(String[] args) {
+        log.error("something wrong here");
+    }
+}
